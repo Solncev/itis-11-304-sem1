3497941d86a65d62de37d96fe7a7dc2ffaa18134
Marat Solncev <maratsolncev@paino.io>
2024-11-08 16:16:59 +0300

    Html + jsp

---
+++ src/main/java/com/solncev/server/LoginServlet.java
@@ -0,0 +1,41 @@
+package com.solncev.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(name = "loginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("login.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        if ("login".equalsIgnoreCase(login) && "password".equals(password)){
+            // session
+            HttpSession httpSession = req.getSession();
+            httpSession.setAttribute("user", login);
+            httpSession.setMaxInactiveInterval(60 * 60);
+
+            // cookie
+            Cookie cookie = new Cookie("user", login);
+            cookie.setMaxAge(24 * 60 * 60);
+            resp.addCookie(cookie);
+            resp.sendRedirect("main.jsp");
+        } else {
+            resp.sendRedirect("/login");
+        }
+    }
+}
